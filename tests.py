2d33514216dff23b9273d792f58a239dc787777e
GiilDe <38991777+gilad212387@users.noreply.github.com>
2019-11-27 14:59:26 +0200

    adding naive and test

--- tests.py
+++ tests.py
@@ -10,13 +10,17 @@ d_type = lambda event: event.symbol == 'ZHNE'
 
 
 if __name__ == "__main__":
+    # Naive seq test
     processor = Processor.Processor("sorted_NASDAQ_20080201_1.txt", Processor.metastock7_attributes,
-                                    Processor.metastock7_time_index, )
+                                    Processor.metastock7_time_index)
     event_types_conditions = [ProcessingUtilities.Condition(func, [i])
                               for i, func in enumerate([a_type, b_type, c_type, d_type])]
     pattern_query = ProcessingUtilities.PatternQuery(ProcessingUtilities.EventPattern(4, ProcessingUtilities.Seq()),
                                                      event_types_conditions, 35)
     left_deep_initializer = GraphBasedProcessingUtilities.LeftDeepTreeInitializer()
     graph_based_processor = GraphBasedProcessingUtilities.GraphBasedProcessing(left_deep_initializer)
-    matches = processor.query(pattern_query, graph_based_processor, "tests_results.txt")
-
+    processor.query(pattern_query, graph_based_processor, "seq_test_results.txt")
+    # Naive and test
+    pattern_query = ProcessingUtilities.PatternQuery(ProcessingUtilities.EventPattern(4, ProcessingUtilities.And()),
+                                                     event_types_conditions, 35)
+    processor.query(pattern_query, graph_based_processor, "and_test_results.txt")
