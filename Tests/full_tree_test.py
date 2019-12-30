2773268921a31c5fa72fce1112830bd295706eea
GiilDe <38991777+gilad212387@users.noreply.github.com>
2019-12-30 20:47:03 +0200

    some changes

--- Tests/full_tree_test.py
+++ Tests/full_tree_test.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
                                     data_formats.metastock7_time_index, data_formats.metastock7_type_index)
     full_deep_initializer = GraphBasedProcessingUtilities.TestingTree()
     full_tree_processor = GraphBasedProcessingUtilities.NaiveMultipleTreesGraphBasedProcessing(full_deep_initializer)
-    processor.query([ProcessingUtilities.CleanPatternQuery(None, [], 50)], full_tree_processor, ProcessingUtilities.TrivialInputInterface(),
+    processor.query([ProcessingUtilities.CleanPatternQuery(None, [], 50)], full_tree_processor,
+                    ProcessingUtilities.TrivialInputInterface(),
                     ProcessingUtilities.FileOutputInterface(["full_tree_test_results.txt"]))
