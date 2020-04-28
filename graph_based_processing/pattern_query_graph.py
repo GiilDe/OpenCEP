3362b36f84b7906ed1b0c289791c4da5b06e8750
GiilDe <38991777+gilad212387@users.noreply.github.com>
2020-04-29 00:22:15 +0300

    small refactoring

---
+++ graph_based_processing/pattern_query_graph.py
@@ -0,0 +1,162 @@
+from typing import List, Union
+import processing_utilities
+
+
+class Node:
+    """
+    Abstract class, predecessor of ConditionNode and EventNode
+    """
+    def __init__(self, memory_model: processing_utilities.MemoryModel, time_limit,
+                 conditions: List[processing_utilities.Condition] = None, parent=None):
+        """
+        :param memory_model: memory mode to save the partial results.
+        :param time_limit: the time limit associated with the query
+        :param conditions: the conditions associated with the query
+        :param parent: predecessor node
+        """
+        if conditions is None:
+            self.conditions = []
+        self.conditions = conditions if conditions else []
+        self.parent = parent
+        self.partial_results_buffer = memory_model
+        self.time_limit = time_limit
+
+    def add_condition(self, condition: processing_utilities.Condition):
+        self.conditions.append(condition)
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+    def get_relevant_results(self, current_time):
+        """
+        :param current_time: time of the current processed event
+        :return: all the partial matches that are still in the time limit
+        """
+        return self.partial_results_buffer.get_relevant_results(current_time, self.time_limit,
+                                                                is_sorted=type(self) == EventNode)
+
+    def get_results(self):
+        """
+        :return: all saved (partial, if node is not root node) matches
+        """
+        return [list(partial_result.completely_unpack().values()) for partial_result in self.partial_results_buffer]
+
+
+class ConditionNode(Node):
+    """
+    Represents an inner node in the graph that holds an operator and a condition list and partial results
+    """
+    def __init__(self, memory_model: processing_utilities.MemoryModel, operator: processing_utilities.Operator,
+                 time_limit, children: List[Node] = None, identifier=None,
+                 conditions: List[processing_utilities.Condition] = None, parent=None):
+        """
+        :param memory_model: memory mode to save the partial results.
+        :param time_limit: the time limit associated with the query
+        :param conditions: the conditions associated with the query
+        :param operator: operator associated with this node
+        :param parent: predecessor node
+        """
+        super().__init__(memory_model, time_limit, conditions, parent)
+        self.children = children
+        self.operator = operator
+        self.identifier = identifier
+
+    def set_output_interface(self, output_interface: processing_utilities.OutputInterface =
+    processing_utilities.TrivialOutputInterface()):
+        self.output_interface = output_interface
+
+    def _check_conditions(self, partial_result: Union[processing_utilities.PartialResult, processing_utilities.Event])\
+            -> bool:
+        """
+        :param partial_result: partial_result to check
+        :return: True if partial result is within the time limit and all conditions hold, False otherwise
+        """
+        return partial_result.end_time - partial_result.start_time <= self.time_limit and \
+               all(condition.check_condition(partial_result) for condition in self.conditions)
+
+    def try_add_partial_result(self, partial_result: processing_utilities.PartialResult, diffuser_child: Node):
+        """
+        Anytime a node succeeds in building a partial results he "diffuses" it to his predecessor to try
+        and further build it
+        :param partial_result: the new partial results
+        :param diffuser_child: the child node that that the new partial results was built in
+        :return: self
+        """
+        current_time = partial_result.start_time
+
+        children_buffers = [child.get_relevant_results(current_time) for child in self.children
+                            if child != diffuser_child]
+        for new_result in self.operator.get_new_results(children_buffers, partial_result, self.identifier):
+            if self._check_conditions(new_result):
+                new_result.operator_type = type(self.operator)
+                self.partial_results_buffer.add_partial_result(new_result)
+                if self.parent:
+                    self.parent.try_add_partial_result(new_result, self)
+
+        if self.is_root() and self.output_interface.output_while_running():
+            results = self.partial_results_buffer.pop_results()
+            self.output_interface.output_results\
+                ([list(partial_result.completely_unpack().values()) for partial_result in results])
+        return self
+
+    def set_children(self, children: List[Node]):
+        self.children = children
+
+    def is_root(self):
+        return self.parent is None
+
+
+class EventNode(Node):
+    """
+    Represents a leaf node in the graph.
+    """
+    def __init__(self, memory_model: processing_utilities.MemoryModel, time_limit,
+                 event_type_and_identifier: processing_utilities.EventTypeOrPatternAndIdentifier,
+                 parent: ConditionNode = None, conditions: List[processing_utilities.Condition] = None):
+        """
+        :param memory_model: memory mode to save the partial results.
+        :param time_limit: the time limit associated with the query
+        :param conditions: the conditions associated with the query
+        :param parent: predecessor node
+        :param event_type_and_identifier: event type associated with this lead node and its matching identifier
+        (in patterns with multiple events of the same type identifiers need to be used)
+        """
+        super().__init__(memory_model, time_limit, conditions, parent)
+        self.event_type = event_type_and_identifier.event_type_or_pattern
+        self.event_identifier = event_type_and_identifier.identifier
+
+    def _check_conditions(self, partial_result: Union[processing_utilities.PartialResult, processing_utilities.Event])\
+            -> bool:
+        return all(condition.check_condition(partial_result) for condition in self.conditions)
+
+    def try_add_partial_result(self, event: processing_utilities.Event):
+        """
+        adds a partial result if it's from the right type
+        :param event: an event corresponding to this leaf node
+        :return: self
+        """
+        if self.event_type == event.get_type():
+            partial_result = processing_utilities.PartialResult({self.event_identifier: event},
+                                                                identifier=self.event_identifier)
+            if self._check_conditions(partial_result):
+                self.partial_results_buffer.add_partial_result(partial_result)
+                self.parent.try_add_partial_result(partial_result, self)
+        return self
+
+
+class PatternQueryGraph:
+    """
+    a graph that represents a pattern query without operator nesting
+    """
+    def __init__(self, root_node: ConditionNode, event_nodes: List[EventNode], use_const_window=False):
+        """
+        :param root_node: graph's root node
+        :param event_nodes: graph's event nodes
+        :param use_const_window: if should use a constant length of events rather than a time limit (if so will treat
+        the time limit paramter as the window size)
+        """
+        self.root_node = root_node
+        self.event_nodes = event_nodes
+        self.use_const_window = use_const_window
+
+
---
+++ processor.py
@@ -0,0 +1,78 @@
+import processing_utilities
+import typing
+from file_sort import sort_file
+
+
+class Processor:
+    """
+    This class represents the main complex event processor
+    """
+    sorted_prefix = 'sorted_'
+
+    def __init__(self, data_file_path: str, attribute_names: typing.List[str], time_attribute_index: int,
+                 type_attribute_index: int, sorted_by_time=True):
+        """
+        initializes all the needed parameters and sorts the input file according to time (if needed)
+        :param data_file_path: input file path
+        :param attribute_names: attribute names of the events in the data file
+        :param time_attribute_index: the index of the time attribute in attribute_names
+        :param type_attribute_index: the index of the event type (name) attribute in attribute_names
+        :param sorted_by_time: whether or not the input file is sorted by the time attribute
+        """
+        self.data_file_path = data_file_path
+        self.attribute_names = attribute_names
+        self.time_name = attribute_names[time_attribute_index]
+        self.type_name = attribute_names[type_attribute_index]
+        if not sorted_by_time:
+            self.data_file_path = Processor.sorted_prefix + self.data_file_path
+            sort_file(time_attribute_index, data_file_path, self.data_file_path)
+
+    def get_event_from_line(self, line):
+        """
+        parses a line from the event input file into an event class
+        :param line: the line from the input file representing to current event
+        """
+        def convert_value(value: str):
+            def isfloat(val: str):
+                try:
+                    float(val)
+                    return True
+                except ValueError:
+                    return False
+            if str.isdigit(value):
+                return int(value)
+            if isfloat(value):
+                return float(value)
+            return value
+        values = line[:-1].split(',')
+        for i, value in enumerate(values):
+            values[i] = convert_value(value)
+        new_event = processing_utilities.Event(self.attribute_names, values, self.time_name, self.type_name)
+        return new_event
+
+    def query(self, pattern_queries: typing.List[processing_utilities.PatternQuery],
+              evaluation_model: processing_utilities.EvaluationModel,
+              input_interface: processing_utilities.InputInterface = processing_utilities.TrivialInputInterface(),
+              output_interfaces: typing.List[processing_utilities.OutputInterface]=None):
+        """
+        creates the evaluation model based on the give queries and the corresponding output interfaces, and parses event
+        lines from the event files and passes them as event objects to the evaluation model
+        :param pattern_queries: the pattern queries to query by
+        :param evaluation_model: the evaluation model to use
+        :param input_interface:
+        :param output_interface:
+        :return:
+        """
+        if output_interfaces is None:
+            output_interfaces = [processing_utilities.TrivialOutputInterface()] * len((pattern_queries))
+        clean_pattern_queries = input_interface.get_clean_pattern_queries(pattern_queries)
+        evaluation_model.set_pattern_queries(clean_pattern_queries, output_interfaces)
+        data_stream = open(self.data_file_path, 'r')
+        counter = 0
+        for line in data_stream:
+            event = self.get_event_from_line(line)
+            evaluation_model.handle_event(event, counter)
+            counter += 1
+        data_stream.close()
+        results = evaluation_model.get_results()
+        return results
