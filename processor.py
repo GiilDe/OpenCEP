310777a0ecc5f4bf54749eb0c27e1516e2408e17
GiilDe <gilad.deutch@gmail.com>
2020-05-27 11:22:42 +0300

    Import changes

--- processor.py
+++ processor.py
@@ -1,8 +1,6 @@
 from . import processing_utilities
 import typing
 from .file_sort import sort_file
-from .graph_based_processing import graph_based_processing_utilities
-import time
 
 
 class Processor:
@@ -79,61 +77,3 @@ class Processor:
         results = evaluation_model.get_results()
         return results
 
-
-class TimeCalcProcessor:
-    def __init__(self, attribute_names: typing.List[str], time_attribute_index: int,
-                 type_attribute_index: int, pattern_queries: typing.List[processing_utilities.PatternQuery]):
-        """
-        initializes all the needed parameters and sorts the input file according to time (if needed)
-        :param data_file_path: input file path
-        :param attribute_names: attribute names of the events in the data file
-        :param time_attribute_index: the index of the time attribute in attribute_names
-        :param type_attribute_index: the index of the event type (name) attribute in attribute_names
-        :param sorted_by_time: whether or not the input file is sorted by the time attribute
-        """
-        self.attribute_names = attribute_names
-        self.time_name = attribute_names[time_attribute_index]
-        self.type_name = attribute_names[type_attribute_index]
-        interface = processing_utilities.TrivialInputInterface()
-        clean_pattern_queries = interface.get_clean_pattern_queries(pattern_queries)
-        self.evaluation_model = \
-            graph_based_processing_utilities.NaiveMultipleTreesGraphBasedProcessing\
-                (graph_based_processing_utilities.LeftDeepTreeInitializer())
-        self.evaluation_model.set_pattern_queries(clean_pattern_queries, [None]*len(clean_pattern_queries))
-
-    def get_event_from_line(self, line):
-        """
-        parses a line from the event input file into an event class
-        :param line: the line from the input file representing to current event
-        """
-        def convert_value(value: str):
-            def isfloat(val: str):
-                try:
-                    float(val)
-                    return True
-                except ValueError:
-                    return False
-            if str.isdigit(value):
-                return int(value)
-            if isfloat(value):
-                return float(value)
-            return value
-        values = line[:-1].split(',')
-        for i, value in enumerate(values):
-            values[i] = convert_value(value)
-        new_event = processing_utilities.Event(self.attribute_names, values, self.time_name, self.type_name)
-        return new_event
-
-    def query(self, events: str):
-        self.evaluation_model.graphs[0].steps_counter.val = 0
-        counter = 0
-        start_time = time.perf_counter_ns()
-        for line in events.split("\n"):
-            event = self.get_event_from_line(line)
-            self.evaluation_model.handle_event(event, counter)
-            counter += 1
-
-        finish_time = time.perf_counter_ns()
-        steps = self.evaluation_model.graphs[0].steps_counter.val
-        self.evaluation_model.clear()
-        return finish_time - start_time, steps
