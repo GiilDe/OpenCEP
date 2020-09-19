417113d88fbf834552c85974c8d3d0efeab63398
Gilad Deutsch <gii@fb.com>
2020-09-19 15:12:23 +0300

    first commit

--- processor.py
+++ processor.py
@@ -81,8 +81,13 @@ class Processor:
 
 
 class TimeCalcProcessor:
+    """
+    This class represents the main complex event processor
+    """
+    sorted_prefix = 'sorted_'
+
     def __init__(self, attribute_names: typing.List[str], time_attribute_index: int,
-                 type_attribute_index: int, pattern_queries: typing.List[processing_utilities.PatternQuery]):
+                 type_attribute_index: int):
         """
         initializes all the needed parameters and sorts the input file according to time (if needed)
         :param data_file_path: input file path
@@ -94,20 +99,53 @@ class TimeCalcProcessor:
         self.attribute_names = attribute_names
         self.time_name = attribute_names[time_attribute_index]
         self.type_name = attribute_names[type_attribute_index]
-        interface = processing_utilities.TrivialInputInterface()
-        clean_pattern_queries = interface.get_clean_pattern_queries(pattern_queries)
-        self.evaluation_model = \
-            graph_based_processing_utilities.NaiveMultipleTreesGraphBasedProcessing\
-                (graph_based_processing_utilities.LeftDeepTreeInitializer())
-        self.evaluation_model.set_pattern_queries(clean_pattern_queries, [None]*len(clean_pattern_queries))
 
-    def query(self, events: str):
+    def get_event_from_line(self, line):
+        """
+        parses a line from the event input file into an event class
+        :param line: the line from the input file representing to current event
+        """
+        def convert_value(value: str):
+            def isfloat(val: str):
+                try:
+                    float(val)
+                    return True
+                except ValueError:
+                    return False
+            if str.isdigit(value):
+                return int(value)
+            if isfloat(value):
+                return float(value)
+            return value
+        values = line[:-1].split(',')
+        for i, value in enumerate(values):
+            values[i] = convert_value(value)
+        new_event = processing_utilities.Event(self.attribute_names, values, self.time_name, self.type_name)
+        return new_event
+
+    def query(self, events: str, pattern_queries: typing.List[processing_utilities.PatternQuery],
+              evaluation_model: processing_utilities.EvaluationModel,
+              input_interface: processing_utilities.InputInterface = processing_utilities.TrivialInputInterface(),
+              output_interfaces: typing.List[processing_utilities.OutputInterface]=None):
+        """
+        creates the evaluation model based on the give queries and the corresponding output interfaces, and parses event
+        lines from the event files and passes them as event objects to the evaluation model
+        :param pattern_queries: the pattern queries to query by
+        :param evaluation_model: the evaluation model to use
+        :param input_interface:
+        :param output_interfaces:
+        :return:
+        """
+        if output_interfaces is None:
+            output_interfaces = [processing_utilities.TrivialOutputInterface()] * len((pattern_queries))
+        clean_pattern_queries = input_interface.get_clean_pattern_queries(pattern_queries)
+        evaluation_model.set_pattern_queries(clean_pattern_queries, output_interfaces)
+
         counter = 0
-        start_time = time.time()
-        for line in events:
+        start_time = time.perf_counter()
+        for line in events.split("\n"):
             event = self.get_event_from_line(line)
-            self.evaluation_model.handle_event(event, counter)
+            evaluation_model.handle_event(event, counter)
             counter += 1
-
-        return time.time() - start_time
-
+        finish_time = time.perf_counter()
+        return finish_time - start_time
